3f78ac794a670b9c6856e2ee13150570e94cf4ac
ishu162002 <ishupurwar.cooper@gmail.com>
2025-08-27 16:55:27 +0530

    Create file.sh

---
+++ file.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo yum update -y
+sudo wget -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+sudo yum install java-17-amazon-corretto -y
+sudo yum install jenkins git -y
+sudo systemct1 enable jenkins
+sudo systemct1 start jenkkins
+sudo systemct1 status jenkins
+sudo mkdir -p /var/tmp_disk
+sudo chmod 1777 /var/tmp_disk
+sudo mount --bind /var/tmp_disk /tmp
+echo '/var/tmp_disk /tmp none bind 0 0' | sudo tee -a /etc/fstab
+sudo systemct1 mask tmp.mount
+df -h/tmp
+sudo systemct1 restart jenkins
